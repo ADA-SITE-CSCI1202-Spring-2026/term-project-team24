d9fc0f3602c331d16f941f8486242ce4221ceab4
zhasmin <zmamedova21316@ada.edu.az>
2026-04-26 19:27:26 +0400

    Add FlightGenerator, update App and Aircraft - Week 2

--- src/model/Aircraft.java
+++ src/model/Aircraft.java
@@ -4,25 +4,31 @@ public abstract class Aircraft {
 
     private String flightNumber;
     private int fuelRequired;
+    private int mealsRequired;
+    private int revenue;
     private int turnaroundTime;
 
-    public Aircraft(String flightNumber, int fuel, int turnaroundTime) {
+    public Aircraft(String flightNumber, int fuelRequired,
+                    int mealsRequired, int revenue, int turnaroundTime) {
         this.flightNumber = flightNumber;
-        this.fuelRequired = fuel;
+        this.fuelRequired = fuelRequired;
+        this.mealsRequired = mealsRequired;
+        this.revenue = revenue;
         this.turnaroundTime = turnaroundTime;
     }
 
-    public String getFlightNumber() {
-        return flightNumber;
-    }
+    public String getFlightNumber()  { return flightNumber; }
+    public int getFuelRequired()     { return fuelRequired; }
+    public int getMealsRequired()    { return mealsRequired; }
+    public int getRevenue()          { return revenue; }
+    public int getTurnaroundTime()   { return turnaroundTime; }
 
-    public int getFuelRequired() {
-        return fuelRequired;
-    }
-
-    public int getTurnaroundTime() {
-        return turnaroundTime;
-    }
-    
     public abstract String getType();
+
+    @Override
+    public String toString() {
+        return getType() + " | " + flightNumber
+                + " | Fuel: " + fuelRequired + "L"
+                + " | Meals: " + mealsRequired;
+    }
 }
