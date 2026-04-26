81007986ced477fdb252dbf30565159e77e1e61e
zhasmin <zmamedova21316@ada.edu.az>
2026-04-26 22:08:10 +0400

    Fixed changes in Aircraft file

--- src/model/Aircraft.java
+++ src/model/Aircraft.java
@@ -4,31 +4,25 @@ public abstract class Aircraft {
 
     private String flightNumber;
     private int fuelRequired;
-    private int mealsRequired;
-    private int revenue;
     private int turnaroundTime;
 
-    public Aircraft(String flightNumber, int fuelRequired,
-                    int mealsRequired, int revenue, int turnaroundTime) {
+    public Aircraft(String flightNumber, int fuel, int turnaroundTime) {
         this.flightNumber = flightNumber;
-        this.fuelRequired = fuelRequired;
-        this.mealsRequired = mealsRequired;
-        this.revenue = revenue;
+        this.fuelRequired = fuel;
         this.turnaroundTime = turnaroundTime;
     }
 
-    public String getFlightNumber()  { return flightNumber; }
-    public int getFuelRequired()     { return fuelRequired; }
-    public int getMealsRequired()    { return mealsRequired; }
-    public int getRevenue()          { return revenue; }
-    public int getTurnaroundTime()   { return turnaroundTime; }
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public int getFuelRequired() {
+        return fuelRequired;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
 
     public abstract String getType();
-
-    @Override
-    public String toString() {
-        return getType() + " | " + flightNumber
-                + " | Fuel: " + fuelRequired + "L"
-                + " | Meals: " + mealsRequired;
-    }
 }
