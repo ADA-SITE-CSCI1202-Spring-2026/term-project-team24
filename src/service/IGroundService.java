91fc931d13ef9620f1c6df180fd72128b7ee28ce
Leyla Khanaliyeva <lkhanaliyeva17147@ada.edu.az>
2026-05-01 23:58:21 +0400

    Update IGroundService.java

--- src/service/IGroundService.java
+++ src/service/IGroundService.java
@@ -5,9 +5,4 @@ import model.*;
 public interface IGroundService {
     boolean canProcess(Aircraft aircraft);
     boolean service(Aircraft aircraft, DepotManager depot);
-import model.Aircraft;
-
-interface IGroundService {
-    boolean canProcess(Aircraft aircraft);
-    void serviceFlight(Aircraft aircraft);
 }
